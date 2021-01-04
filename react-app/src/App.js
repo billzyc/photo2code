bf5b3e401b86efa1180e2cdd8bb9afe586d88128
billzyc <billcshto@gmail.com>
2021-01-04 00:19:07 -0500

    move pages into components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,25 @@
 import { CookiesProvider } from "react-cookie";
 
 import "./App.scss";
-import Login from "./pages/Login/Login.js";
-import Landing from "./pages/Landing/Landing.js";
-import Upload from "./pages/Upload/Upload.js";
+import Landing from "./components/Landing/Landing.js";
+import Upload from "./components/Upload/Upload.js";
 import NavBar from "./components/NavBar/NavBar.js";
 import UserContextProvider from "./contexts/UserContext";
 import FilesContextProvider from "./contexts/FilesContext";
 
 function App() {
   return (
-    <div className="app">
-      <CookiesProvider>
-        <UserContextProvider>
-          <FilesContextProvider>
+    <CookiesProvider>
+      <UserContextProvider>
+        <FilesContextProvider>
+          <div className="app">
             <NavBar />
             <Landing />
             <Upload />
-          </FilesContextProvider>
-        </UserContextProvider>
-      </CookiesProvider>
-    </div>
+          </div>
+        </FilesContextProvider>
+      </UserContextProvider>
+    </CookiesProvider>
   );
 }
 
