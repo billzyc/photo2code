7e75810302bbf8eb47d18316f0a8dee35af6b581
Bill <billcshto@gmail.com>
2021-01-20 03:34:30 -0500

    remove unused imports

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,12 +1,9 @@
-import React, { useContext } from "react";
-import { UserContext } from "../../contexts/UserContext";
+import React from "react";
 import GoogleOAuth from "../GoogleOAuth/GoogleOAuth";
 
 import "./NavBar.scss";
-import Profile from "../Profile/Profile";
 
 function NavBar() {
-  const { userEmail, userID } = useContext(UserContext);
   return (
     <div className="navBar">
       <h3>{"<Photo2Code>"}</h3>
